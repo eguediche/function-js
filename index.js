048722414d8660d34e01de0fe3b413829d647cd1
eguediche <eguediche@edenschool.fr>
2024-11-06 09:02:51 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// exo 1 :
+
+function addition(a, b){
+
+    return a + b
+  }
+  console.log(addition(2, 3));
+
+  // exo 2 :
+
+  function salutation(dfgh) {
+  
+    return "Bonjour, " + dfgh
+    
+  }
+  console.log(salutation("Eden"));
+
+  // exo 3 :
+
+  
